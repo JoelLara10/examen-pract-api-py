76562caeaa779d8beda69ae2927ab6dc4783f20e
Joel <joel020105@gmail.com>
2025-04-11 12:52:12 -0600

    first commit

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -43,7 +43,7 @@ def login():
         print("Credenciales inválidas")  # 👈 debug
         return jsonify({"msg": "Credenciales inválidas"}), 401
 
-    access_token = create_access_token(identity=user.id)
+    access_token = create_access_token(identity=str(user.id))
     return jsonify(access_token=access_token), 200
 
 
